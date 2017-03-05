9f726051031e5d04c89d3e0ff30d61082cbd4a75
Henny022 <hen.geheim22@gmail.com>
2017-03-05 22:34:40 +0100

    renamed some classes

--- src/main/java/de/teamhug/GlacialEpoch/Tileentities/GE_TileEntityCampFire.java
+++ src/main/java/de/teamhug/GlacialEpoch/Tileentities/GE_TileEntityCampFire.java
@@ -1,21 +1,10 @@
-package de.teamhug.GlacialEpoch.Tileentities;
+package de.teamhug.GlacialEpoch.TileEntities;
 
-import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockFurnace;
-import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemHoe;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemSword;
-import net.minecraft.item.ItemTool;
 import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
