f7f204d32dacbb84d895b8b947263df063270805
Rohrschneider <rohrschneider83@gmail.com>
2017-03-16 22:37:47 +0100

    rohrschneider
    test

--- src/main/java/de/teamhug/GlacialEpoch/Registry/GE_ItemRegistry.java
+++ src/main/java/de/teamhug/GlacialEpoch/Registry/GE_ItemRegistry.java
@@ -10,11 +10,8 @@ import net.minecraft.item.Item;
  */
 public class GE_ItemRegistry
 {
-<<<<<<< HEAD
+
     //Butcher
-=======
-    //ButcherItem
->>>>>>> origin/master
 	public static Item itemCadaverCow;		//Kuhkörper
 	public static Item itemBovine_half; 	//Rinderhälfte
 	public static Item itemBovine_skin; 	//Rinderhaut
@@ -25,10 +22,7 @@ public class GE_ItemRegistry
 	public static Item itemHidesCutter;
 	public static Item itemCuttingKnife;
 	
-<<<<<<< HEAD
-	
-=======
->>>>>>> origin/master
+
 	//Tools
 	public static Item itemPrimitiveTool;
 
@@ -114,11 +108,7 @@ public class GE_ItemRegistry
         itemCowHead.setCreativeTab(GE_CreativeTabs.tabGEButcher);
         GameRegistry.registerItem(itemCowHead, "itemCowHead");
         
-<<<<<<< HEAD
 
-=======
-
->>>>>>> origin/master
         //ButcherTools
         itemHidesCutter = new GE_Item("GE_itemHidesCutter");
         itemHidesCutter.setMaxStackSize(1);
@@ -133,17 +123,16 @@ public class GE_ItemRegistry
         itemSlaughteringAx = new GE_Item("GE_itemSlaughteringAx");
         itemSlaughteringAx.setMaxStackSize(1);
         itemSlaughteringAx.setCreativeTab(GE_CreativeTabs.tabGEButcher);
-<<<<<<< HEAD
+
         GameRegistry.registerItem(itemSlaughteringAx, "GE_itemSlaughteringAx");        
 
         
         //Tools
-=======
+
         GameRegistry.registerItem(itemSlaughteringAx, "GE_itemSlaughteringAx");
         
         //Tools
         
->>>>>>> origin/master
         itemPrimitiveTool = new GE_Item("GE_itemPrimitiveTool");
         itemPrimitiveTool.setMaxStackSize(1);
         GameRegistry.registerItem(itemPrimitiveTool, "GE_itemPrimitiveTool");
