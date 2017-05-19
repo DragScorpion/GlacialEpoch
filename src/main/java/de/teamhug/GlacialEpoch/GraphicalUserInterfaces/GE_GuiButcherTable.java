9fa045e3c3d7e61065cd9ce67707b0e04350c21c
Henny022 <info@henny022.de>
2017-05-19 15:39:46 +0200

    structure update
    fixed typo in GraphicalUserInterfaces package
    moved psd file out of src because we dont need it in our mod

---
+++ src/main/java/de/teamhug/GlacialEpoch/GraphicalUserInterfaces/GE_GuiButcherTable.java
@@ -0,0 +1,47 @@
+package de.teamhug.GlacialEpoch.GraphicelUserInterfaces;
+
+import org.lwjgl.opengl.GL11;
+
+import de.teamhug.GlacialEpoch.GE_Main;
+import de.teamhug.GlacialEpoch.Container.GE_ContainerButcherTable;
+import de.teamhug.GlacialEpoch.Container.GE_ContainerCampFire;
+import de.teamhug.GlacialEpoch.TileEntities.GE_TileEntityButcherTable;
+import de.teamhug.GlacialEpoch.TileEntities.GE_TileEntityCampFire;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.util.ResourceLocation;
+
+public class GE_GuiButcherTable extends GuiContainer {
+	
+	public final ResourceLocation texture = new ResourceLocation(GE_Main.MODID, "textures/gui/GE_guiButcherTable.png");
+	public final GE_TileEntityButcherTable tileEntity;
+	
+	public GE_GuiButcherTable(InventoryPlayer inventory, GE_TileEntityButcherTable te) {
+		super(new GE_ContainerButcherTable(inventory, te));
+		this.tileEntity = te;
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_) {
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        this.mc.getTextureManager().bindTexture(texture);
+        this.xSize = 250;
+        this.ySize = 196;
+        int k = (this.width - this.xSize) / 2;
+        int l = (this.height - this.ySize) / 2;
+        this.guiLeft = k;
+        this.guiTop = l;
+        this.drawTexturedModalRect(k,l, 0, 0, this.xSize, this.ySize);
+        
+        if (this.tileEntity.isBurning())
+        {
+            int i1 = this.tileEntity.getBurnTimeRemainingScaled(13);
+            this.drawTexturedModalRect(k + 107, l + 65 + 12 - i1, 243, 256 - i1, 14, i1 + 1);
+            i1 = this.tileEntity.getCookProgressScaled(31);
+            this.drawTexturedModalRect(k + 109, l + 36, 0, 237, i1 + 1, 19);
+        }
+		
+	}
+	
+}
---
+++ src/main/java/de/teamhug/GlacialEpoch/GraphicalUserInterfaces/GE_GuiHandler.java
@@ -0,0 +1,56 @@
+package de.teamhug.GlacialEpoch.GraphicelUserInterfaces;
+
+import cpw.mods.fml.common.network.IGuiHandler;
+import de.teamhug.GlacialEpoch.Container.GE_ContainerButcherTable;
+import de.teamhug.GlacialEpoch.Container.GE_ContainerCampFire;
+import de.teamhug.GlacialEpoch.Registry.GE_GuiRegistry;
+import de.teamhug.GlacialEpoch.TileEntities.GE_TileEntityButcherTable;
+import de.teamhug.GlacialEpoch.TileEntities.GE_TileEntityCampFire;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class GE_GuiHandler implements IGuiHandler {
+	
+	@Override
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		TileEntity te = world.getTileEntity(x, y, z);
+		
+		switch(ID) {
+		case GE_GuiRegistry.guiCampFire:
+			//System.out.println("Test Container 1");
+			if (te instanceof GE_TileEntityCampFire) {
+				//System.out.println("Test Container 2");
+				return new GE_ContainerCampFire(player.inventory, (GE_TileEntityCampFire)te);
+			}
+			break;
+		case GE_GuiRegistry.guiButcherTable:
+			if (te instanceof GE_TileEntityButcherTable) {
+				return new GE_ContainerButcherTable(player.inventory, (GE_TileEntityButcherTable)te);
+			}
+			break;
+		}
+		return null;
+	}
+
+	@Override
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		TileEntity te = world.getTileEntity(x, y, z);
+		
+		switch(ID) {
+		case GE_GuiRegistry.guiCampFire:
+			//System.out.println("Test GUI 1");
+			if (te instanceof GE_TileEntityCampFire) {
+				//System.out.println("Test GUI 2");
+				return new GE_GuiCampFire(player.inventory, (GE_TileEntityCampFire)te);
+			}
+			break;
+		case GE_GuiRegistry.guiButcherTable:
+			if (te instanceof GE_TileEntityButcherTable) {
+				return new GE_GuiButcherTable(player.inventory, (GE_TileEntityButcherTable)te);
+			}
+			break;
+		}
+		return null;
+	}
+}
